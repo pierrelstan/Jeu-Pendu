d694885dc5070e30e49fc826827fd3096e95e930
@pierrelstan <stanleypl75@gmail.com>
2018-12-29 11:05:33 -0500

    edit the randomWords

--- src/App.js
+++ src/App.js
@@ -11,15 +11,17 @@ import WinOrLost from './components/WinOrLost';
 const LETTER = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 const RandomWords = [
   {
-    words: ["ABC"],
+    words: ["A","B","C"],
     title: "ALPHABET"
   },
   {
-    words: ["PORCHE"],
+    words: ["P", "O", "R",
+      "C", "H","E"],
     title: "Car"
   },
   {
-    words: ["YATCH"],
+    words: ["Y", "A", "T", "C",
+      "H"],
     title: "BOAT"
   },
   /* and so on... */
@@ -48,6 +50,7 @@ constructor(){
       //console.log(RandomWords[random].words)
       return RandomWords[random].words 
     }
+    
     
   // Arrow fx for binding
   handleClick = (index)=> { 
